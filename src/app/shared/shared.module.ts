403aab40f008ccd58d59a983e9c4a0de46dc6231
Ed Lee <ronder.lee@gmail.com>
2017-12-11 20:38:20 +0800

    善用 barrels, 直接import from '目錄' (index.ts)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,8 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-import { FooterComponent } from './layout/footer.component';
-import { HeaderComponent } from './layout/header.component';
+import { FooterComponent, HeaderComponent } from './layout';
 
 @NgModule({
   imports: [
